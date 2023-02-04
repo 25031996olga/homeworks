bb9d95fdc7c7527dc6f166d3b70b868ee9160c12
Воленька <olga25_73@mail.ru>
2023-02-04 18:27:33 +0300

    work

---
+++ main.py
@@ -0,0 +1,18 @@
+а = int(input())
+в= int(input())
+C= int(input())
+
+
+if
+     а+b=> c and a+c>=b and b+c >=a  :
+     a > = 1 :
+     b > = 1 :
+     c> = 1 :
+
+
+    print ('cуществует')
+else:
+    print('не существует')
+
+
+
